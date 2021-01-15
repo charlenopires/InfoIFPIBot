c97416c73bfeeee2b9ea0f8f0b422f1c14714513
Charleno Pires <charleno@gmail.com>
2021-01-14 23:25:20 -0300

    Organizando os TODOS

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 /** tem como toda vez que alguem postaar um link o bot perguntar para qual categoria ele associa o link.
 Tipo: cursos / palestras / offtopic / assuntos internos */
 
-TODO: Montar Firebase
-TODO: CRUD de Categoria
-TODO: Analisar se msgs são links e reply com possíveis categorias
-TODO: Guardar links com categoria no Firebase
-TODO: Subir para Heroku como cron
+//TODO: Montar Firebase
+//TODO: CRUD de Categoria
+//TODO: Analisar se msgs são links e reply com possíveis categorias
+//TODO: Guardar links com categoria no Firebase
+//TODO: Subir para Heroku como cron
 
 const { Telegraf } = require('telegraf')
 const bot = new Telegraf('1543897523:AAEh7haKaRAcy1ofLrzCjmJzfIMtk1fvJHM', {username: 'infoifpibot'})
